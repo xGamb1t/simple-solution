00f267c2a0c930ae1711a24fb60640a94f1daeec
Konstantin <56113575+xGamb1t@users.noreply.github.com>
2021-03-04 14:58:41 +0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -29,10 +29,7 @@
             }
         }
     });
-$("#contactForm").submit(function(event){
-    // cancels the form submission
-    event.preventDefault();
-    submitForm();
+
 });
     // Closes responsive menu when a scroll trigger link is clicked
     $(".js-scroll-trigger").click(function () {
